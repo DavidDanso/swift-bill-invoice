c6b5e7f968c7f0b53a28a3f8f51ac81e1522a585
DavidDanso <desiingerboy@icloud.com>
2024-03-20 12:41:51 +0000

    Added migrations folder to .gitignore

--- swift_bill/settings.py
+++ swift_bill/settings.py
@@ -98,24 +98,24 @@ admin, or simply use the PostgreSQL DB.
 '''
 
 # sqlite3 DB
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # postgresql DB
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': config('DB_NAME'),
-       'USER': config('DB_USER'),
-       'PASSWORD': config('DB_PASSWORD'),
-       'HOST': config('DB_HOST'),
-       'PORT': config('DB_PORT')
-   }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': config('DB_NAME'),
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST'),
+#        'PORT': config('DB_PORT')
+#    }
+# }
 
 
 # Password validation
