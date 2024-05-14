c9e1aeb41d6e69b6c585f99cc28dcab2a9b4d12c
DavidDanso <desiingerboy@icloud.com>
2024-05-14 15:08:22 +0000

    collectstatic

--- staticfiles/js/invoice.js
+++ staticfiles/js/invoice.js
@@ -1,3 +1,36 @@
+$(document).ready(function () {
+  /********************************************** Navigation ********************************************/
+
+  //closes responsive menu when a scroll trigger link is clicked
+  $(".nav-link").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  //
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 70,
+  });
+
+  //On scroll down if offset is higher than 50 pixels add navBar shrink else remove that class
+  $(window).scroll(function () {
+    if ($("#mainNav").offset().top > 50) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  });
+
+  //
+  $(window).scroll(function () {
+    if ($("#mainNav").offset().top > 1000) {
+      $("#mainNav").addClass("navbar-color");
+    } else {
+      $("#mainNav").removeClass("navbar-color");
+    }
+  });
+});
+
 //live-search
 jQuery(document).ready(function ($) {
   $(".live-search-list #search_invoice").each(function () {
