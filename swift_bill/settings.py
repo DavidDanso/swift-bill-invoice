5115ec9bc5fe4372dbd77365425212bebbad0cb3
DavidDanso <desiingerboy@icloud.com>
2024-05-14 11:25:19 +0000

    cloudinary

--- swift_bill/settings.py
+++ swift_bill/settings.py
@@ -120,7 +120,7 @@ DATABASES = {
     }
 }
 
-POSTGRES_LOCALLY = False
+POSTGRES_LOCALLY = True
 if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
